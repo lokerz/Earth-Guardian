ca3fecb2d7ebf44f890f1eba82998918830a099f
LokerZ <ridwan.abdurrasyid@yahoo.com>
2018-10-19 18:39:28 +0700

    v1

---
+++ Assets/Manager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour {
+	[Range(1.0f, 1.2f)]
+	public float KeySize = 1.0f;
+	public bool inGame = false;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Option.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Option : MonoBehaviour {
+	private Manager manager;
+	void Start(){
+		manager = GameObject.Find ("Manager").GetComponent<Manager> ();
+	}
+		
+}
